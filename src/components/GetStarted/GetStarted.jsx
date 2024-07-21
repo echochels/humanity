11c0ed04d827accf6f006e2d8fa96345c4df5d8e
echochels <ajahchelsea@gmail.com>
2024-07-21 10:02:03 +0100

    account

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import './GetStarted.css'
+import React, { useState } from 'react';
+import './GetStarted.css';
 import { JackInTheBox } from 'react-awesome-reveal';
 
 const GetStarted = () => {
+  const [isPopupVisible, setPopupVisible] = useState(false);
+
+  const togglePopup = () => {
+    setPopupVisible(!isPopupVisible);
+  };
+
   return (
-    <section className="g-wrapper">
+    <div className="g-wrapper">
       <JackInTheBox>
-      <div className="paddings innerWidth g-container" id='getstarted'>
-        <div className="flexColCenter inner-container">
-          <span className='primaryText'>Donate to IYIATATA Youth Empowerment Initiative</span>
-          <span className='secondaryText'>Together, we are making a difference and driving positive change in our community.
-            <br />  Join us in our mission to empower and uplift!</span>
-          <button className='button'>
-            <a href="">Click to Donate</a>
-          </button>
+        <div className={`paddings innerWidth g-container ${isPopupVisible ? 'blurred' : ''}`} id='getstarted'>
+          <div className="flexColCenter inner-container">
+            <span className='primaryText'>Donate to IYIATATA Youth Empowerment Initiative</span>
+            <span className='secondaryText'>Together, we are making a difference and driving positive change in our community.
+              <br />  Join us in our mission to empower and uplift!</span>
+            <button className='button' onClick={togglePopup}>
+              Click to Donate
+            </button>
+          </div>
         </div>
-      </div>
       </JackInTheBox>
-    </section>
-  )
+
+      {isPopupVisible && (
+        <div className="popup">
+          <div className="popup-content">
+            <span className="close" onClick={togglePopup}>&times;</span>
+            <h2 style={{ color: "#4066ff" }}>Account Details</h2>
+            <p>Account Name:  IYIATATA YOUTH EMPOWERMENT INITIATIVE</p>
+            <p>Account Number: 1312673779</p>
+            <p>Bank: Zenith Bank</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default GetStarted
+export default GetStarted;
